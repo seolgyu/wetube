b6ba1253c73add77bd6fd7f63fff04ac1d298085
kim seol gyu <sgk0418on@naver.com>
2019-02-21 04:21:31 +0900

    babel, nodemon commit

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express'); //exprss를 호출하고
+import express from "express";//const express = require('express'); exprss를 호출하고
 const app = express(); //add에 담고
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () => 
+console.log(`Listening on: http://localhost:${PORT}`);
+/*function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
-}
+}*/
+
+
+const handleHome = (req, res) => res.send("hello from my ass");
+/*function handleHome(req, res) {
+    res.send("hello from home"); //그것에 대한 응답하는 방식이지!
+}*/
+
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+/*function handleProfile(req, res){
+    res.send("You are on my profile"); 그것에 대한 응답하는 방식이지!
+}*/
+
+app.get("/", handleHome); //기본적인 라우트!
+
+app.get("/profile", handleProfile); //기본적인 라우트!
 
 app.listen(PORT, handleListening); //4000 port를  listen
