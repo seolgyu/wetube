1114e7e8a137ef86d49d8278883d68c7b0aa108f
kim seol gyu <sgk0418on@naver.com>
2019-02-22 17:52:13 +0900

    commit

--- app.js
+++ app.js
@@ -3,18 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router"; //export 한걸 import
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express(); //add에 담고
-
-
-const handleHome = (req, res) => res.send("hello from my ass");
-/*function handleHome(req, res) {
-    res.send("hello from home"); //그것에 대한 응답하는 방식
-}*/
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -22,12 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-
-app.get("/", handleHome); //기본적인 라우트!
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter); // use(해당Router을 전부 사용) 
-
+app.use("/video", videoRouter);
 
 export default app;
--- router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-export const userRouter = express.Router(); //export(default 아님)
-
-userRouter.get("/",(req,res) => res.send("user index")); //user로 접근시 "/"여기home
-userRouter.get("/edit",(req,res) => res.send("user edit"));//user/edit 접근시
-userRouter.get("/password",(req,res) => res.send("user passwoed"));//user/pasword 접근시
