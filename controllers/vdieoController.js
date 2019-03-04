c564d88cc373e60d49dfe3b1082546efa7955e96
kim seol gyu <sgk0418on@naver.com>
2019-03-05 01:38:33 +0900

    commit

--- controllers/vdieoController.js
+++ controllers/vdieoController.js
@@ -1,9 +1,17 @@
 //globalRouter
 export const home = (req, res) => res.render("home", {pageTitle: "Home" });
-export const search = (req, res) => res.render("Search", {pageTitle: "Serch" } );
+
+export const search = (req, res) => {res.render
+    console.log(req.query);
+    ("Search", {pageTitle: "Serch" } );
+}
+
 
 //videolRouter
 export const upload = (req, res) => res.render("Upload", {pageTitle: "Upload" } );
+
 export const videoDetail = (req, res) => res.render("Video Detail", {pageTitle: "Video Detail" } );
+
 export const editVideo = (req, res) => res.render("Edit Video", {pageTitle: "Edit Video" } );
+
 export const deleteVideo = (req, res) => res.render("Delete Video", {pageTitle: "Delete Video" } );
